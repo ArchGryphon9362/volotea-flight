cc1e4b18824ba0059db0b6656531390a8cee3a8c
IannFinn <114349844+IannFinn@users.noreply.github.com>
2023-07-14 14:22:57 +0100

    Removed unnecessary prints

--- main.py
+++ main.py
@@ -5,32 +5,31 @@ import requests
 import time
 
 flight_data = requests.get("https://play.volotea.com/api/flightinfo/?display_language=EN").json()
-print("--- Aircraft Info ---")
-print(f"Registration: {flight_data['aircraft']}")
+print(f"""--- Aircraft Info ---
+Registration: {flight_data['aircraft']}""")
 
-print("--- Flight Info ---")
-print(f"Flight Id: {flight_data['flight_id']}")
-print(f"Origin City: {flight_data['origin_airport']['city']}")
-print(f"Origin Airport: {flight_data['origin_airport']['iata_code']} - {flight_data['origin_airport']['name']}")
-print(f"Origin Latitude: {flight_data['origin_airport']['latitude']}")
-print(f"Origin Longitude: {flight_data['origin_airport']['longitude']}")
-print(f"Destination City: {flight_data['destination_airport']['city']}")
-print(f"Destination Airport: {flight_data['destination_airport']['iata_code']} - {flight_data['destination_airport']['name']}")
-print(f"Destination Latitude: {flight_data['destination_airport']['latitude']}")
-print(f"Destination Longitude: {flight_data['destination_airport']['longitude']}")
+print(f"""--- Flight Info ---
+Flight Id: {flight_data['flight_id']}
+Origin City: {flight_data['origin_airport']['city']}
+Origin Airport: {flight_data['origin_airport']['iata_code']} - {flight_data['origin_airport']['name']}
+Origin Latitude: {flight_data['origin_airport']['latitude']}
+Origin Longitude: {flight_data['origin_airport']['longitude']}
+Destination City: {flight_data['destination_airport']['city']}
+Destination Airport: {flight_data['destination_airport']['iata_code']} - {flight_data['destination_airport']['name']}")
+Destination Latitude: {flight_data['destination_airport']['latitude']}
+Destination Longitude: {flight_data['destination_airport']['longitude']}""")
 
 while True:
-	print("--- New Flight Data ---")
-	print(f"Latitude: {flight_data['latitude']}")
-	print(f"Longitude: {flight_data['longitude']}")
-	print(f"Pitch: {flight_data['pitch']}")
-	print(f"Roll: {flight_data['roll']}")
-	print(f"Yaw: {flight_data['yaw']}")
-	print(f"Heading Angle: {flight_data['heading']}")
-	print(f"Speed: Mach {flight_data['mach_speed']}")
-	print(f"Ground Speed: {flight_data['ground_speed']} knots")
-	print(f"Altitude: {flight_data['altitude']}ft")
-	print(f"Outside Temp: {flight_data['outside_temperature']}C")
-	
+	print(f"""--- New Flight Data ---
+Latitude: {flight_data['latitude']}
+Longitude: {flight_data['longitude']}
+Pitch: {flight_data['pitch']}
+Roll: {flight_data['roll']}
+Yaw: {flight_data['yaw']}
+Heading Angle: {flight_data['heading']}
+Speed: Mach {flight_data['mach_speed']}
+Ground Speed: {flight_data['ground_speed']} knots
+Altitude: {flight_data['altitude']}ft
+Outside Temp: {flight_data['outside_temperature']}C""")
 	time.sleep(5)
 	flight_data = requests.get("https://play.volotea.com/api/flightinfo/?display_language=EN").json()
